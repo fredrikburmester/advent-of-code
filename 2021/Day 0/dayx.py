b60f20f61d74b2678b656996180227d092262f7e
Fredrik Burmester <fredrik.burmester@gmail.com>
2021-12-23 22:43:39 +0100

    blueprint for aoc python file

--- 2021/Day 0/dayx.py
+++ 2021/Day 0/dayx.py
@@ -3,18 +3,108 @@
 from collections import defaultdict
 from copy import deepcopy
 import time
+from typing import Union
 
 
-def neighbours_4(matrix, x, y):
+def neighbours_4(m: Union[list, defaultdict], x, y) -> list:
+    if not isinstance(m, list) and not isinstance(m, defaultdict):
+        assert False, "neighbours_4 only works with list or defaultdict"
+    if len(m) == 0:
+        assert False, "Empty input in neighbours_4"
+
     def in_range(x, y):
-        return 0 <= x < len(matrix[0]) and 0 <= y < len(matrix)
+        if isinstance(m, list):
+            return 0 <= x < len(m[0]) and 0 <= y < len(m)
+        else:
+            return min(m)[0] <= x <= max(m)[0] and min(m)[1] <= y <= max(m)[1]
     return [p for p in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)] if in_range(*p)]
 
 
-def neighbours_8(matrix, x, y):
+def neighbours_8(m: Union[list, defaultdict], x, y) -> list:
+    if not isinstance(m, list) and not isinstance(m, defaultdict):
+        assert False, "neighbours_8 only works with list or defaultdict"
+    if len(m) == 0:
+        assert False, "Empty input in neighbours_4"
+
     def in_range(x, y):
-        return 0 <= x < len(matrix[0]) and 0 <= y < len(matrix)
+        if isinstance(m, list):
+            return 0 <= x < len(m[0]) and 0 <= y < len(m)
+        else:
+            return min(m)[0] <= x <= max(m)[0] and min(m)[1] <= y <= max(m)[1]
     return [p for p in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1), (x+1, y+1), (x-1, y-1), (x+1, y-1), (x-1, y+1)] if in_range(*p)]
+
+
+def min_max_of_set(input_set: set) -> list:
+    """ Returns the min and max x and y values of a set """
+    min_x, max_x = float('inf'), 0
+    min_y, max_y = float('inf'), 0
+    for s in input_set:
+        if s[0] < min_x:
+            min_x = s[0]
+        if s[0] > max_x:
+            max_x = s[0]
+        if s[1] < min_y:
+            min_y = s[1]
+        if s[1] > max_y:
+            max_y = s[1]
+
+    return [min_x, max_x, min_y, max_y]
+
+
+def print_dict_full(d: Union[dict, defaultdict, set]) -> None:
+    """ Pretty print a dictionary or set | print, dict, set, defaultdict """
+    _max = []
+    _min = []
+    if isinstance(d, set):
+        min_x, max_x, min_y, max_y = min_max_of_set(d)
+        _max.append(max_x)
+        _max.append(max_y)
+        _min.append(min_x)
+        _min.append(min_y)
+
+        print(_min, _max)
+        for y in range(0, _max[1] + 1):
+            for x in range(0, _max[0] + 1):
+                if (x, y) in d:
+                    print("#", end="")
+                else:
+                    print(".", end="")
+            print()
+    else:
+        _max = max(d)
+        _min = min(d)
+        for y in range(0, _max[1] + 1):
+            for x in range(0, _max[0] + 1):
+                print(d.get((x, y), "."), end="")
+            print()
+
+
+def print_dict(d: Union[dict, defaultdict, set]) -> None:
+    """ Pretty print a dictionary or set | print, dict, set, defaultdict """
+    _max = []
+    _min = []
+    if isinstance(d, set):
+        min_x, max_x, min_y, max_y = min_max_of_set(d)
+        _max.append(max_x)
+        _max.append(max_y)
+        _min.append(min_x)
+        _min.append(min_y)
+
+        print(_min, _max)
+        for y in range(_min[1], _max[1] + 1):
+            for x in range(_min[1], _max[0] + 1):
+                if (x, y) in d:
+                    print("#", end="")
+                else:
+                    print(".", end="")
+            print()
+    else:
+        _max = max(d)
+        _min = min(d)
+        for y in range(_min[1], _max[1] + 1):
+            for x in range(_min[1], _max[0] + 1):
+                print(d.get((x, y), "."), end="")
+            print()
 
 
 def help_func():
@@ -25,6 +115,7 @@ def part1(input_list):
     """ Part 1"""
 
     mapp = defaultdict(int)
+    s = set()
 
     first = input_list[0]
     lx = len(first)
@@ -54,7 +145,7 @@ def part2(input_list):
 
 
 def main():
-    with open("./2021/Day x/input.txt", "r", encoding='UTF-8') as file:
+    with open("./2021/Day 24/input.txt", "r", encoding='UTF-8') as file:
         input_list = [str(line.strip()) for line in file]
 
     t0 = time.time()
